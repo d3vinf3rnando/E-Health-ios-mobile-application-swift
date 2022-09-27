0b5920cd6d394bdb84cdd05efcf6512b156c4c5c
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-09-28 00:06:32 +0530

    Initial Commit

---
+++ E-Health/E_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_HealthApp.swift
+//  E-Health
+//
+//  Created by Devin Fernando on 2022-09-28.
+//
+
+import SwiftUI
+
+@main
+struct E_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
